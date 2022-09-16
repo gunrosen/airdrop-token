c1ba856731049f8dcfcf2e3dfb48367356ced89f
Hung Pham <phamhungbkhn@gmail.com>
2022-09-16 17:06:37 +0700

    update: airdrop test3

---
+++ src/const.js
@@ -0,0 +1,9 @@
+module.exports = {
+    WEB3_API_URL : {
+        ETH: '',
+        BSC: 'https://bsc-dataseed.binance.org/',
+        POLYGON: 'https://polygon-rpc.com/',
+        BSC_TEST: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+        POLYGON_MUMBAI: 'https://matic-mumbai.chainstacklabs.com/',
+    }
+}
